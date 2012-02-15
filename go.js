114cae817333a6d1c2fe704104b56fb57dff7973
Greg <elorian.mestec@gmail.com>
2012-02-15 09:54:31 -0800

    Detection of trying to place a piece on an opponent's now working

--- go.js
+++ go.js
@@ -95,6 +95,23 @@ function placePiece(x,y,color) {
                    
 }
 
+//switiches the current player state and updates the gameboard
+//accordingly
+function switchPlayer() {
+   printBoard();//this is just diagnostic it doesn't need to be here
+
+   if (currentPlayer == "black") {
+        console.log("Switching from black to white player");
+        currentPlayer = "white";
+        $('#currentPlayer').text("Next to Play: White");
+   }
+   else {
+      currentPlayer = "black";
+      console.log("Switching from white to black player");
+      $('#currentPlayer').text("Next to Play: Black");
+   }
+}
+
 //this function takes raw canvas coordinates and converts them
 //into the nearest node on the go board that the user clicked on
 function clickToGoCoordinates(mouseX, mouseY) {
@@ -112,36 +129,68 @@ function clickToGoCoordinates(mouseX, mouseY) {
     if (goY > 19) goY=19;
 
     console.log("Go coordinates clicked on: " + goX + "," +goY);
-    attemptMove(goX, goY, "black");
+    attemptMove(goX, goY, currentPlayer);
 }
 
 //attempts to make a move and alerts the player if it's
 //illegal or something's wrong, etc.
 function attemptMove(x,y,player) {
-      placePiece(x,y,player);
-      switchPlayer();
-
+      if (makeMove(x,y, player)) {
+        placePiece(x,y,player);
+        switchPlayer();
+      }
+      else {
+        console.log("couldn't make the move for some reason");
+      }
 }
 
 
-//game state 
+//establish a closure to store the gameboard
+//and binds its return to gbfunc
+var gbfunc = function () {
+        var board = new Array(19);
+        for (i=0; i < 19; i++) {
+            board[i] = new Array(19);
+        }
 
+        //this function goes through the logic of a move and determines whether
+        //or not it is a legal move, updating the game state iff it is
+        function makeMoveFunction(x,y,player) {
+            if(board[x-1][y-1] == null) {
+                board[x-1][y-1] = player;
+                return true;
+            }
+            else if (false) {
+            //this is where the ko rule would be implemented
+
+            }
+            else {
+                return false;
+            }
+
+        }
+
+        return {makeMove: makeMoveFunction, 
+                removePiece: function(x,y) {
+                        console.log("Removed a piece of color " + board[x-1][y-1] + "at " + x+","+y);
+                        board[x-1][y-1] = ""; },
+
+                printBoard: function () { console.log(board);},
+        };
+}();
+
+//parcel out individual functions from the gbfunc wrapper
+//and bind them to variables in the global namespace
+var makeMove = gbfunc.makeMove;
+var removePiece = gbfunc.removePiece;
+var printBoard = gbfunc.printBoard;
+
+
+
+//currentPlayer is the global arbitor of whose turn it is
+$('#currentPlayer').text("Next to Play: Black");
 var currentPlayer = "black";//initialized to black on game start 
                             //because that's how you play Go 
-$('#currentPlayer').text("Next to Play: Black");
-
-function switchPlayer() {
-   if (currentPlayer == "black") {
-        console.log("Switching from black to white player");
-        currentPlayer = "white";
-        $('#currentPlayer').text("Next to Play: White");
-   }
-   else {
-      currentPlayer = "black";
-      console.log("Switching from white to black player");
-      $('#currentPlayer').text("Next to Play: Black");
-   }
-}
 
 
 
@@ -155,6 +204,4 @@ canvas.addEventListener('click', function (e) {
      console.log("Click at: " + e.offsetX + "," + e.offsetY);
      clickToGoCoordinates(e.offsetX,e.offsetY);
 });
-
-
 
